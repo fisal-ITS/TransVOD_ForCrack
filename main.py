29ff5c5abf8fb256dc0b675743ad3734217048a7
Fisal Aly <90088354+fisal-ITS@users.noreply.github.com>
2023-06-01 11:23:58 +0700

    Update main.py

--- main.py
+++ main.py
@@ -269,8 +269,8 @@ def main(args):
 	                    param.requires_grad = True
 					else:
 	                    param.requires_grad = False
-				missing_keys, unexpected_keys = model_without_ddp.load_state_dict(tmp_dict, strict=False)
-
+				
+		missing_keys, unexpected_keys = model_without_ddp.load_state_dict(tmp_dict, strict=False)
         unexpected_keys = [k for k in unexpected_keys if not (k.endswith('total_params') or k.endswith('total_ops'))]
         if len(missing_keys) > 0:
             print('Missing Keys: {}'.format(missing_keys))
@@ -285,15 +285,15 @@ def main(args):
 
     print("Start training")
     start_time = time.time()
-    writer = SummaryWriter()
+    writer = SummaryWriter('/content/tensorboard/')
     for epoch in range(args.start_epoch, args.epochs):
         if args.distributed:
             sampler_train.set_epoch(epoch)
         train_stats = train_one_epoch(
             model, criterion, data_loader_train, optimizer, device, epoch, args.clip_max_norm)
 		loss = train_stats['loss']
-		writer.add_scalar("Loss/train", loss, epoch)
-        lr_scheduler.step()
+		writer.add_scalar("Training Loss", loss, epoch)
+		lr_scheduler.step()
         print('args.output_dir', args.output_dir)
         if args.output_dir:
             checkpoint_paths = [output_dir / 'checkpoint.pth']
